994edbefdff005818117bd416c034aa2e1b94423
Jina <wonjieunkk@gmail.com>
2022-05-30 22:27:43 +0900

    Movie Type

--- server.js
+++ server.js
@@ -43,6 +43,25 @@ const typeDefs = gql`
   }
   type Movie {
     id: ID!
+    url: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
   }
   type Query {
     allMovies: [Movie!]!
